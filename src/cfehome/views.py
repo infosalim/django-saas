67caa9253d22d1e0d0440ddb1479a589d75ff0d1
infosalim <salimh5757@gmail.com>
2024-10-15 00:01:20 +0600

    prepare for railway

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -6,13 +6,13 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
     queryset = PageVisit.objects.filter(path=request.path)
     my_title = "Django SaaS App"
-    my_context = {
-        "page_title": my_title,
-        "page_visit_count": queryset.count()
-    }
+    my_context = {"page_title": my_title, "page_visit_count": queryset.count()}
     html_template = "home.html"
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
